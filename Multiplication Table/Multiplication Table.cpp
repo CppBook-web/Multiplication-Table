983379de33b4ebb4a91a2299689999bef565e783
workshopdolgikh <workshopdolgikh@gmail.com>
2023-03-14 09:36:17 +0300

    Добавьте файлы проекта.

---
+++ Multiplication Table/Multiplication Table.cpp
@@ -0,0 +1,24 @@
+﻿//Таблица умножения
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    std::cout << "\n\t\t\tТАБЛИЦА УМНОЖЕНИЯ\n";
+
+    int row = 10; //ряды
+    int col = 10; //столбцы
+
+    for (int i = 1; i <= row; i++) {
+        std::cout << "\n\n\t";
+        for (int j = 1; j <= col; j++) {
+            int mul = i * j;
+            std::cout << mul << "\t";
+        }
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
